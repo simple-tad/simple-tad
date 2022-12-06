f948fc7548b86e60f50fd42fdf0774de3b8e0044
Xyphuz <hansen0715.2@gmail.com>
2022-12-06 21:19:12 +0800

    chore: transepose input matrix and process

--- src/di.hpp
+++ src/di.hpp
@@ -1,12 +1,13 @@
+#include "constants.h"
 #include <algorithm>
 #include <fstream>
+#include <immintrin.h>
 #include <iostream>
 #include <map>
 #include <numeric>
 #include <sstream>
 #include <string>
 #include <vector>
-#include "constants.h"
 
 const std::tuple<double*, std::size_t> read_hi_c_data(const std::string& filename, const std::size_t& bin_size, const std::size_t& bin1_min, const std::size_t& bin1_max, const std::size_t& bin2_min, const std::size_t& bin2_max) {
     std::fstream file;
@@ -57,38 +58,103 @@ const std::tuple<double*, std::size_t> read_hi_c_data(const std::string& filenam
     return std::make_tuple(data, edge_size);
 }
 
-std::vector<double> calculate_di(const double* contact_matrix, const std::size_t& edge_size, const std::size_t& bin_size) {
+std::vector<double> calculate_di(double* contact_matrix, const std::size_t& edge_size, const std::size_t& bin_size) {
     std::size_t range = SIGNIFICANT_BINS / bin_size;
+    std::vector<double> A(edge_size, 0);
+    std::vector<double> B(edge_size, 0);
+    std::vector<double> E(edge_size, 0);
     std::vector<double> di(edge_size, 0);
 
-    for (std::size_t locus_index = 0; locus_index < edge_size; locus_index++) {
-        double A;
-        double B;
-        if (locus_index < range) {
-            // edge case
-            A = std::accumulate(contact_matrix + locus_index * edge_size, contact_matrix + locus_index * edge_size + locus_index, 0.0);
-            B = std::accumulate(contact_matrix + locus_index * edge_size + locus_index + 1, contact_matrix + locus_index * edge_size + locus_index + range + 1, 0.0);
-        } else if (locus_index >= edge_size - range) {
-            // edge case
-            A = std::accumulate(contact_matrix + locus_index * edge_size + locus_index - range, contact_matrix + locus_index * edge_size + locus_index, 0.0);
-            B = std::accumulate(contact_matrix + locus_index * edge_size + locus_index + 1, contact_matrix + (locus_index + 1) * edge_size, 0.0);
-        } else {
-            // normal case
-            A = std::accumulate(contact_matrix + locus_index * edge_size + locus_index - range, contact_matrix + locus_index * edge_size + locus_index, 0.0);
-            B = std::accumulate(contact_matrix + locus_index * edge_size + locus_index + 1, contact_matrix + locus_index * edge_size + locus_index + range + 1, 0.0);
-        }
+    for (std::size_t row = 0; row < range; row++) {
+        double a = std::accumulate(contact_matrix + row * edge_size + row - range, contact_matrix + row * edge_size + row, 0.0);
+        double b = std::accumulate(contact_matrix + row * edge_size + row + 1, contact_matrix + row * edge_size + row + range + 1, 0.0);
+        A[row] = a;
+        B[row] = b;
+        E[row] = (a + b) / 2;
+    }
 
-        double E = (A + B) / 2;
+    for (std::size_t row = (edge_size - range) + 1; row < edge_size; row++) {
+        double a = std::accumulate(contact_matrix + row * edge_size + row - range, contact_matrix + row * edge_size + row, 0.0);
+        double b = std::accumulate(contact_matrix + row * edge_size + row + 1, contact_matrix + row * edge_size + row + range + 1, 0.0);
+        A[row] = a;
+        B[row] = b;
+        E[row] = (a + b) / 2;
+    }
 
-        if (A == 0 && B == 0) {
-            di[locus_index] = 0;
-        } else {
-            try {
-                di[locus_index] = ((B - A) / (std::abs(B - A))) * ((((A - E) * (A - E)) / E) + (((B - E) * (B - E)) / E));
-            } catch (std::exception& e) {
-                di[locus_index] = 0;
-            }
+    // align diagonal data
+    for (std::size_t row = range; row < (edge_size - range) + 1; row++) {
+        for (std::size_t col = 0; col < 2 * range; col++) {
+            contact_matrix[row * edge_size + col] = contact_matrix[row * edge_size + col + (row - range)];
         }
     }
+
+    // transponse matrix
+    double* transposed_matrix = new double[edge_size * edge_size];
+    for (std::size_t row = 0; row < edge_size; row++) {
+        for (std::size_t col = 0; col < edge_size; col++) {
+            transposed_matrix[row * edge_size + col] = contact_matrix[col * edge_size + row];
+        }
+    }
+
+    // use _mm256_add_pd to calculate A, B and E
+    for (std::size_t col = range; col < (edge_size - range) + 1; col += 4) {
+        for (std::size_t row = 0; row < range - 1; row++) {
+            __m256d vA = _mm256_loadu_pd(transposed_matrix + row * edge_size + col);
+            __m256d vA_next = _mm256_loadu_pd(transposed_matrix + (row + 1) * edge_size + col);
+            _mm256_storeu_pd(transposed_matrix + (row + 1) * edge_size + col, _mm256_add_pd(vA, vA_next));
+        }
+
+        A[col] = transposed_matrix[range * edge_size + col];
+        A[col + 1] = transposed_matrix[range * edge_size + col + 1];
+        A[col + 2] = transposed_matrix[range * edge_size + col + 2];
+        A[col + 3] = transposed_matrix[range * edge_size + col + 3];
+
+        for (std::size_t row = range; row < 2 * range - 1; row++) {
+            __m256d vB = _mm256_loadu_pd(transposed_matrix + row * edge_size + col);
+            __m256d vB_next = _mm256_loadu_pd(transposed_matrix + (row + 1) * edge_size + col);
+            _mm256_storeu_pd(transposed_matrix + (row + 1) * edge_size + col, _mm256_add_pd(vB, vB_next));
+        }
+
+        B[col] = transposed_matrix[2 * range * edge_size + col];
+        B[col + 1] = transposed_matrix[2 * range * edge_size + col + 1];
+        B[col + 2] = transposed_matrix[2 * range * edge_size + col + 2];
+        B[col + 3] = transposed_matrix[2 * range * edge_size + col + 3];
+
+        E[col] = (A[col] + B[col]) / 2;
+        E[col + 1] = (A[col + 1] + B[col + 1]) / 2;
+        E[col + 2] = (A[col + 2] + B[col + 2]) / 2;
+        E[col + 3] = (A[col + 3] + B[col + 3]) / 2;
+    }
+
+    for (std::size_t row = 0; row < edge_size; row += 4) {
+        __m256d vA = _mm256_loadu_pd(A.data() + row);
+        __m256d vB = _mm256_loadu_pd(B.data() + row);
+        __m256d vE = _mm256_loadu_pd(E.data() + row);
+
+        __m256d vAB = _mm256_sub_pd(vB, vA);
+        __m256d vAE = _mm256_sub_pd(vA, vE);
+        __m256d vBE = _mm256_sub_pd(vB, vE);
+
+        // (B - A) / (std::abs(B - A))
+        __m256d vSign = _mm256_cmp_pd(vAB, _mm256_setzero_pd(), _CMP_GT_OQ);
+        vSign = _mm256_sub_pd(_mm256_mul_pd(vSign, _mm256_set1_pd(2)), _mm256_set1_pd(1));
+        __m256d vSignBA2 = _mm256_div_pd(vAB, vAB);
+        vSignBA2 = _mm256_mul_pd(vSignBA2, vSign);
+
+        // (((A - E) * (A - E)) / E)
+        __m256d vAE2 = _mm256_mul_pd(vAE, vAE);
+        __m256d vAE2E = _mm256_div_pd(vAE2, vE);
+
+        // (((B - E) * (B - E)) / E)
+        __m256d vBE2 = _mm256_mul_pd(vBE, vBE);
+        __m256d vBE2E = _mm256_div_pd(vBE2, vE);
+
+        // ((B - A) / (std::abs(B - A))) * ((((A - E) * (A - E)) / E) + (((B - E) * (B - E)) / E));
+        __m256d vResult = _mm256_add_pd(vAE2E, vBE2E);
+        vResult = _mm256_mul_pd(vResult, vSignBA2);
+
+        _mm256_storeu_pd(di.data() + row, vResult);
+    }
+
     return di;
 }
