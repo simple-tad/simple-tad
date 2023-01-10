1fb19851b8f54044d68af5123a8ce7fd0b6a8cb2
jimyang <jimyang890724@gmail.com>
2023-01-10 19:39:22 +0800

    feat: forward, backward with simd.

--- src/lib/baum_welch_simd.hpp
+++ src/lib/baum_welch_simd.hpp
@@ -31,26 +31,26 @@ double inline log_sum(double const* arr, std::size_t n) {
     return max + std::log(acc);
 }
 
-__m256d inline log_sum_AVX(__m256d const* arr, std::size_t n) {
-    // double max = *std::max_element(arr, arr + n);
-    __m256d max_AVX = _mm256_set1_pd(-std::numeric_limits<double>::infinity());
+// __m256d inline log_sum_AVX(__m256d const* arr, std::size_t n) {
+//     // double max = *std::max_element(arr, arr + n);
+//     __m256d max_AVX = _mm256_set1_pd(-std::numeric_limits<double>::infinity());
 
-    // if (max == std::numeric_limits<double>::infinity()) {
-    //     return max;
-    // }
-    max_AVX = _mm256_max_pd(arr[0], _mm256_max_pd(arr[1], arr[2]));
+//     // if (max == std::numeric_limits<double>::infinity()) {
+//     //     return max;
+//     // }
+//     max_AVX = _mm256_max_pd(arr[0], _mm256_max_pd(arr[1], arr[2]));
 
-    // double acc = 0;
-    __m256d acc_AVX = _mm256_set1_pd(0);
+//     // double acc = 0;
+//     __m256d acc_AVX = _mm256_set1_pd(0);
 
-    for (std::size_t i = 0; i < n; ++i) {
-        // acc += std::exp(arr[i] - max);
-        acc_AVX = _mm256_add_pd(acc_AVX, _mm256_exp_pd(_mm256_sub_pd(arr[i], max_AVX)));
-    }
+//     for (std::size_t i = 0; i < n; ++i) {
+//         // acc += std::exp(arr[i] - max);
+//         acc_AVX = _mm256_add_pd(acc_AVX, _mm256_exp_pd(_mm256_sub_pd(arr[i], max_AVX)));
+//     }
 
-    // return max + std::log(acc);
-    return _mm256_add_pd(max_AVX, _mm256_log_pd(acc_AVX));
-}
+//     // return max + std::log(acc);
+//     return _mm256_add_pd(max_AVX, _mm256_log_pd(acc_AVX));
+// }
 
 const std::tuple<const double, const double*> forward(
     const int* observations,
@@ -61,45 +61,31 @@ const std::tuple<const double, const double*> forward(
     const std::size_t num_states,
     const std::size_t num_emissions) {
     // init the alpha matrix
-    double* alpha = new double[num_states * num_observations]();
+    double* alpha = new (std::align_val_t(32)) double[num_states * num_observations]();
     for (std::size_t i = 0; i < num_states; ++i) {
         alpha[i * num_observations] = initial[i] + emission[i * num_emissions + observations[0]];
     }
 
     // compute the alpha matrix
     for (std::size_t t = 1; t < num_observations; ++t) {
-        if (num_states == 3) {
-            double* buf = new (std::align_val_t(32)) double[12]; // 3 * 4
-            __m256d buf_AVX;
-            for (std::size_t prev_state = 0; prev_state < num_states; ++prev_state) {
-                __m256d prev_alpha_AVX = _mm256_set1_pd(alpha[prev_state * num_observations + t - 1]);
-                __m256d trans_AVX = _mm256_load_pd(transition + prev_state * num_states);
-                buf_AVX = _mm256_add_pd(prev_alpha_AVX, trans_AVX);
-                _mm256_store_pd(buf + prev_state * 4, buf_AVX);
-            }
-            auto swap = [&buf](auto const& pos1, auto const& pos2) {
-                auto tmp = buf[pos2];
-                buf[pos2] = buf[pos1];
-                buf[pos1] = tmp;
-            };
-            swap(1, 4); swap(2, 8); swap(6, 9);
-            alpha[t] = log_sum(buf, num_states) + emission[observations[t]];
-            alpha[1 * num_observations + t] = log_sum(buf + 4, num_states) + emission[1 * num_emissions + observations[t]];
-            alpha[2 * num_observations + t] = log_sum(buf + 8, num_states) + emission[2 * num_emissions + observations[t]];
-            delete[] buf;
+        double* buf = new (std::align_val_t(32)) double[12]; // 3 * 4
+        __m256d buf_AVX;
+        for (std::size_t prev_state = 0; prev_state < num_states; ++prev_state) {
+            __m256d prev_alpha_AVX = _mm256_set1_pd(alpha[prev_state * num_observations + t - 1]);
+            __m256d trans_AVX = _mm256_load_pd(transition + prev_state * num_states);
+            buf_AVX = _mm256_add_pd(prev_alpha_AVX, trans_AVX);
+            _mm256_store_pd(buf + prev_state * 4, buf_AVX);
         }
-
-        else {
-            // now is observation t, and t is from 1 because we skip init observation
-            for (std::size_t curr_state = 0; curr_state < num_states; ++curr_state) {
-                double* buf = new double[num_states];
-                for (std::size_t prev_state = 0; prev_state < num_states; ++prev_state) {
-                    buf[prev_state] = alpha[prev_state * num_observations + t - 1] + transition[prev_state * num_states + curr_state];
-                }
-                alpha[curr_state * num_observations + t] = log_sum(buf, num_states) + emission[curr_state * num_emissions + observations[t]];
-                delete[] buf;
-            }
-        }
+        auto swap = [&buf](auto const& pos1, auto const& pos2) {
+            auto tmp = buf[pos2];
+            buf[pos2] = buf[pos1];
+            buf[pos1] = tmp;
+        };
+        swap(1, 4); swap(2, 8); swap(6, 9);
+        alpha[t] = log_sum(buf, num_states) + emission[observations[t]];
+        alpha[1 * num_observations + t] = log_sum(buf + 4, num_states) + emission[1 * num_emissions + observations[t]];
+        alpha[2 * num_observations + t] = log_sum(buf + 8, num_states) + emission[2 * num_emissions + observations[t]];
+        delete[] buf;
     }
 
     // compute the probability of the observation sequence
@@ -124,21 +110,40 @@ const double* backward(
     const std::size_t num_states,
     const std::size_t num_emissions) {
     // init the beta matrix
-    double* beta = new double[num_states * num_observations]();
+    double* beta = new (std::align_val_t(32)) double[num_states * num_observations]();
     for (std::size_t i = 0; i < num_states; ++i) {
         beta[i * num_observations + num_observations - 1] = 0; // log(1)
     }
 
     // compute the beta matrix
     for (std::size_t t = num_observations - 2;; --t) {
-        // now is observation t, and t is from (num_observations - 2) because we already init last observation
-        for (std::size_t prev_state = 0; prev_state < num_states; ++prev_state) {
-            double* buf = new double[num_states];
-            for (std::size_t curr_state = 0; curr_state < num_states; ++curr_state) {
-                buf[curr_state] = beta[curr_state * num_observations + t + 1] + transition[prev_state * num_states + curr_state] + emission[curr_state * num_emissions + observations[t + 1]];
-            }
-            beta[prev_state * num_observations + t] = log_sum(buf, num_states);
+        double* buf = new (std::align_val_t(32)) double[12]; // 3 * 4
+        __m256d buf_AVX;
+        for (std::size_t curr_state = 0; curr_state < num_states; ++curr_state) {
+            __m256d curr_beta_AVX = _mm256_set1_pd(beta[curr_state * num_observations + t + 1]);
+            __m256d trans_AVX = _mm256_set_pd(
+                transition[curr_state],
+                transition[1 * num_states + curr_state],
+                transition[2 * num_states + curr_state],
+                -std::numeric_limits<double>::infinity());
+            __m256d emit_AVX = _mm256_set1_pd(emission[curr_state * num_emissions + observations[t + 1]]);
+            buf_AVX = _mm256_add_pd(_mm256_add_pd(curr_beta_AVX, trans_AVX), emit_AVX);
+            _mm256_store_pd(buf + curr_state * 4, buf_AVX);
         }
+        // std::cout << "buf: " << t << std::endl
+            // << buf[0] << ' ' << buf[1] << ' ' << buf[2] << ' ' << buf[3] << std::endl
+            // << buf[4] << ' ' << buf[5] << ' ' << buf[6] << ' ' << buf[7] << std::endl
+            // << buf[8] << ' ' << buf[9] << ' ' << buf[10] << ' ' << buf[11] << std::endl << std::endl;
+        auto swap = [&buf](auto const& pos1, auto const& pos2) {
+            auto tmp = buf[pos2];
+            buf[pos2] = buf[pos1];
+            buf[pos1] = tmp;
+        };
+        swap(1, 11); swap(2, 7); swap(5, 10);
+        beta[t] = log_sum(buf + 1, num_states);
+        beta[1 * num_observations + t] = log_sum(buf + 5, num_states);
+        beta[2 * num_observations + t] = log_sum(buf + 9, num_states);
+        delete[] buf;
 
         if (t == 0) {
             break;
@@ -299,6 +304,8 @@ void baum_welch(
             std::cout << "Converged at iteration " << iter << std::endl;
             break;
         }
+
+        // std::cout << std::abs(alpha_sum - previous_alpha_sum) << std::endl;
         previous_alpha_sum = alpha_sum;
 
         ++iter;
